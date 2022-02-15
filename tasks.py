f938d39ef42542d831fab03b1ecc1e568e6cd090
AkhmedLostInShali <Akhmedik89@gmail.com>
2022-02-15 22:09:06 +0300

    №7 results/nickname/level/rating

--- tasks.py
+++ tasks.py
@@ -203,5 +203,34 @@ def anketa():
                 </html>"""
 
 
+@app.route("/results/<nickname>/<int:level>/<float:rating>")
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h4>Результаты отбора</h4>
+                    <h2>Претендента на участие в миссии {nickname}:</h2>
+                    <div class="alert alert-success" role="alert">
+                      Поздравляем! Ваш рейтинг после {str(level)} этапа отбора
+                    </div>
+                    <div class="alert alert-light" role="alert">
+                      составляет {str(rating)}!
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      Желаем удачи!
+                    </div>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
